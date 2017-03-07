1ed09034bde357437cb052c7f0f9814153a71630
dean <dean@deandeimac.thenetcircle.lab>
2017-03-07 13:35:53 +0800

    add snap demo

--- app/src/main/java/org/dean/pull2refreshandsnap/MainActivity.java
+++ app/src/main/java/org/dean/pull2refreshandsnap/MainActivity.java
@@ -1,13 +1,91 @@
 package org.dean.pull2refreshandsnap;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.LinearSnapHelper;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView rv;
+    private List<Integer> mDataList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rv = (RecyclerView) findViewById(R.id.rv);
+        mDataList = new ArrayList<>();
+        mDataList.add(R.mipmap.pic_1);
+        mDataList.add(R.mipmap.pic_2);
+        mDataList.add(R.mipmap.pic_3);
+        mDataList.add(R.mipmap.pic_4);
+        mDataList.add(R.mipmap.pic_5);
+        mDataList.add(R.mipmap.pic_6);
+        mDataList.add(R.mipmap.pic_1);
+        mDataList.add(R.mipmap.pic_2);
+        mDataList.add(R.mipmap.pic_3);
+        mDataList.add(R.mipmap.pic_4);
+        mDataList.add(R.mipmap.pic_5);
+        mDataList.add(R.mipmap.pic_6);
+        mDataList.add(R.mipmap.pic_1);
+        mDataList.add(R.mipmap.pic_2);
+        mDataList.add(R.mipmap.pic_3);
+        mDataList.add(R.mipmap.pic_4);
+        mDataList.add(R.mipmap.pic_5);
+        mDataList.add(R.mipmap.pic_6);
+        mDataList.add(R.mipmap.pic_1);
+        mDataList.add(R.mipmap.pic_2);
+        mDataList.add(R.mipmap.pic_3);
+        mDataList.add(R.mipmap.pic_4);
+        mDataList.add(R.mipmap.pic_5);
+        mDataList.add(R.mipmap.pic_6);
+
+        rv.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        rv.setAdapter(new CardRvAdapter(this, mDataList));
+
+        // LinearSnapHelper mLinearSnapHelper = new LinearSnapHelper();
+        // once move one item
+        LinearSnapHelper mLinearSnapHelper = new LinearSnapHelper() {
+            @Override
+            public int findTargetSnapPosition(RecyclerView.LayoutManager layoutManager, int velocityX, int velocityY) {
+                View centerView = findSnapView(layoutManager);
+                if (centerView == null)
+                    return RecyclerView.NO_POSITION;
+
+                int position = layoutManager.getPosition(centerView);
+                int targetPosition = -1;
+                if (layoutManager.canScrollHorizontally()) {
+                    if (velocityX < 0) {
+                        targetPosition = position - 1;
+                    } else {
+                        targetPosition = position + 1;
+                    }
+                }
+
+                if (layoutManager.canScrollVertically()) {
+                    if (velocityY < 0) {
+                        targetPosition = position - 1;
+                    } else {
+                        targetPosition = position + 1;
+                    }
+                }
+
+                final int firstItem = 0;
+                final int lastItem = layoutManager.getItemCount() - 1;
+                targetPosition = Math.min(lastItem, Math.max(targetPosition, firstItem));
+                return targetPosition;
+            }
+        };
+        mLinearSnapHelper.attachToRecyclerView(rv);
+
+//        CustomSnapHelper mMySnapHelper = new CustomSnapHelper();
+//        mMySnapHelper.attachToRecyclerView(rv);
     }
 }
+
