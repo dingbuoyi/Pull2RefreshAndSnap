863f2e61e0018ca687bded6899d208190f04d7e0
dean <dean@deandeimac.thenetcircle.lab>
2017-03-07 14:01:21 +0800

    add pull 2 refresh scrollview

--- app/src/main/java/org/dean/pull2refreshandsnap/MainActivity.java
+++ app/src/main/java/org/dean/pull2refreshandsnap/MainActivity.java
@@ -6,19 +6,32 @@ import android.view.View;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.LinearSnapHelper;
 import android.support.v7.widget.RecyclerView;
+import android.widget.ScrollView;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import in.srain.cube.views.ptr.PtrClassicFrameLayout;
+import in.srain.cube.views.ptr.PtrDefaultHandler;
+import in.srain.cube.views.ptr.PtrFrameLayout;
+import in.srain.cube.views.ptr.PtrHandler;
 
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView rv;
     private List<Integer> mDataList;
+    private PtrClassicFrameLayout mPtrFrame;
+    private ScrollView mScrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initPull2Refresh();
+        initSnap();
+    }
+
+    private void initSnap() {
         rv = (RecyclerView) findViewById(R.id.rv);
         mDataList = new ArrayList<>();
         mDataList.add(R.mipmap.pic_1);
@@ -86,6 +99,45 @@ public class MainActivity extends AppCompatActivity {
 
 //        CustomSnapHelper mMySnapHelper = new CustomSnapHelper();
 //        mMySnapHelper.attachToRecyclerView(rv);
+
+    }
+
+    private void initPull2Refresh() {
+        mScrollView = (ScrollView) findViewById(R.id.rotate_header_scroll_view);
+        mPtrFrame = (PtrClassicFrameLayout) findViewById(R.id.rotate_header_web_view_frame);
+        mPtrFrame.setLastUpdateTimeRelateObject(this);
+        mPtrFrame.setPtrHandler(new PtrHandler() {
+            @Override
+            public boolean checkCanDoRefresh(PtrFrameLayout frame, View content, View header) {
+                return PtrDefaultHandler.checkContentCanBePulledDown(frame, mScrollView, header);
+            }
+
+            @Override
+            public void onRefreshBegin(PtrFrameLayout frame) {
+                mPtrFrame.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mPtrFrame.refreshComplete();
+                    }
+                }, 100);
+            }
+        });
+
+        // the following are default settings
+        mPtrFrame.setResistance(1.7f);
+        mPtrFrame.setRatioOfHeaderHeightToRefresh(1.2f);
+        mPtrFrame.setDurationToClose(200);
+        mPtrFrame.setDurationToCloseHeader(1000);
+        // default is false
+        mPtrFrame.setPullToRefresh(false);
+        // default is true
+        mPtrFrame.setKeepHeaderWhenRefresh(true);
+        mPtrFrame.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mPtrFrame.autoRefresh();
+            }
+        }, 100);
     }
 }
 
